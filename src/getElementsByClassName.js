d35e5b6643bf1086dad8e70bb7ab20d1fc8951d5
groverpankaj <pgrover2079@gmail.com>
2019-12-09 12:53:59 -0800

    GetElementsByClassName attempt

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -5,6 +5,41 @@
 
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className
-) {
-  // your code here
-};
+  ) {
+    // your code here
+    let result = [];
+
+    let currElem;
+
+    // first will be document.body , afterwards it will be given child element
+    if(arguments[1] === undefined) {
+      currElem = document.body;
+    } else {
+      currElem = arguments[1];
+    }
+
+    // get classname of the current element
+    let allClasses = currElem.className;
+
+    if (allClasses !== undefined) {   // Undefined - No class
+      let allClassesArray = allClasses.split(' ');
+
+      for(let i = 0; i < allClassesArray.length; i++) {
+        if(allClassesArray[i] === className) {
+          result.push(currElem); break;
+        }
+      }
+    }
+
+    let childElem = currElem.childNodes;
+
+    // child nodes
+
+    //iterate through each child nodes
+    for(let i = 0; i < childElem.length; i++) {
+      // call for recursion on each child element
+      result = result.concat(getElementsByClassName(className, childElem[i]));
+    }
+
+      return result;
+  };
