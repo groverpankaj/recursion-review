a0241be5f9aad3143d3e1eacc7f5772479a53251
groverpankaj <pgrover2079@gmail.com>
2019-12-09 20:33:28 -0800

    parseJSON attempt - not complete

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -2,6 +2,54 @@
 // var parseJSON = JSON.parse;
 
 // but you're not, so you'll write it from scratch:
+const findEnd = function(input) {
+
+  let result;
+
+  // Initialize
+  let openBracket = '{';
+  let closeBracket = '}';
+
+  if(input[0] === '[') {
+    openBracket = '[';
+    closeBracket = ']';
+  }
+
+  let openCount = 1;
+  for(let i = 1; i < input.length; i++) {
+    console.log(closeBracket)
+    if(input[i] === openBracket) {openCount++;}
+    if(input[i] === closeBracket) {openCount--;}
+    if(openCount === 0) {result = i; console.log(i); break;}
+  }
+
+  return result;
+
+}
+
+
 var parseJSON = function(json) {
   // your code goes here
+  if(Number(json) == json) {
+    return (Number(json));
+  }
+
+  if(json.slice(0,4) === 'true') {
+    return true;
+  }
+
+  if(json.slice(0,5) === 'false') {
+    return false;
+  }
+
+  if(json.slice(0,4) === 'null') {
+    return null;
+  }
+
+  if(json[0] === '[') {
+    let closePos = findEnd(json);
+
+  }
+
+
 };
